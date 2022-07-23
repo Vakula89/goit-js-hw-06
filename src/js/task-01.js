c680316c23b62d57b591c4b6d992ddf57d2b19da
Vakula89 <vasyl.yento@gmail.com>
2022-07-23 13:10:01 +0300

    first task

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+
+const listEl = document.querySelector("ul#categories");
+const listItemEl = listEl.querySelectorAll(".item");
+
+console.log(`Number of categories: ${listItemEl.length}`);
+
+listItemEl.forEach(element => {
+    console.log(`Category: ${element.querySelector("h2").textContent}`);
+    console.log(`Elements: ${element.querySelectorAll("li").length}`);
+});
+
+
+
