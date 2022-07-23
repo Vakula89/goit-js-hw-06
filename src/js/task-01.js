f4dd6ace1a2df945d1ffa9f931bad6ccc25260b8
Vakula89 <vasyl.yento@gmail.com>
2022-07-23 14:13:21 +0300

    task 2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -8,6 +8,3 @@ listItemEl.forEach(element => {
     console.log(`Category: ${element.querySelector("h2").textContent}`);
     console.log(`Elements: ${element.querySelectorAll("li").length}`);
 });
-
-
-
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = ingredients.map(ingredient => {
+
+	const liEl = document.createElement("li");
+	liEl.textContent = ingredient;
+	liEl.className = "item";
+	return liEl;
+});
+
+document.querySelector("ul#ingredients").append(...ingredientsList);
