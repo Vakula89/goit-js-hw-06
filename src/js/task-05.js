03b9dacf0b70824acdb241f7b854b683bd33967d
Vakula89 <vasyl.yento@gmail.com>
2022-07-26 16:31:08 +0300

    Update Task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,16 +1,8 @@
-const inputEl = document.getElementById("name-input");
-const outputEl = document.getElementById("name-output");
-
-inputEl.addEventListener("input", updateOutputEl);
-
-function updateOutputEl() {
-	outputEl.textContent = inputEl.value == null ? "Anonymous" : inputEl.value;
-}
-// const input = document.getElementById("name-input");
-// const output = document.getElementById("name-output");
-// input.addEventListener("keyup", (ev) => {
-//     output.innerText = ev.target.value;
-//     if(ev.target.value === ''){
-//         output.innerText = 'Anonymous';
-//     }
-// })
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+input.addEventListener("keyup", (event) => {
+    output.innerText = event.target.value;
+    if(event.target.value === ''){
+        output.innerText = 'Anonymous';
+    }
+})
